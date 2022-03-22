1f00f91aea65e9d88e7285b363088b3dc2f3f941
Marximus Maximus <marximus@marximus.com>
2022-03-22 16:23:08 -0700

    Add regions, adjust comments, variable renames, symlinks now forced, some shellcheck ignores

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
+################################################################################
+#region anti-sourceing
+
 sourced=0
 if [ -n "$ZSH_EVAL_CONTEXT" ]; then 
   case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
 elif [ -n "$KSH_VERSION" ]; then
-  [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
+  # shellcheck disable=SC2296
+  [ "$(cd "$(dirname -- "$0")" && pwd -P)/$(basename -- "$0")" != "$(cd "$(dirname -- "${.sh.file}")" && pwd -P)/$(basename -- "${.sh.file}")" ] && sourced=1
 elif [ -n "$BASH_VERSION" ]; then
   (return 0 2>/dev/null) && sourced=1 
 else # All other shells: examine $0 for known shell binary filenames
@@ -16,32 +20,43 @@ if [ $sourced -eq 1 ]; then
   exit 1
 fi
 
-function rreadlink() ( # Execute the function in a *subshell* to localize variables and the effect of `cd`.
+#endregion anti-sourceing
+################################################################################
 
-  target=$1 fname= targetDir= CDPATH=
+################################################################################
+#region Functions
+
+rreadlink() ( # Execute the function in a *subshell* to localize variables and the effect of `cd`.
+
+  target=$1 
+  fname= 
+  targetDir= 
+  CDPATH=
 
   # Try to make the execution environment as predictable as possible:
   # All commands below are invoked via `command`, so we must make sure that `command`
   # itself is not redefined as an alias or shell function.
-  # (Note that command is too inconsistent across shells, so we don't use it.)
+  # (NOTE: that command is too inconsistent across shells, so we don't use it.)
   # `command` is a *builtin* in bash, dash, ksh, zsh, and some platforms do not even have
   # an external utility version of it (e.g, Ubuntu).
   # `command` bypasses aliases and shell functions and also finds builtins 
   # in bash, dash, and ksh. In zsh, option POSIX_BUILTINS must be turned on for that
   # to happen.
   { \unalias command; \unset -f command; } >/dev/null 2>&1
+  # shellcheck disable=SC2034
   [ -n "$ZSH_VERSION" ] && options[POSIX_BUILTINS]=on # make zsh find *builtins* with `command` too.
 
   while :; do # Resolve potential symlinks until the ultimate target is found.
       [ -L "$target" ] || [ -e "$target" ] || { command printf '%s\n' "ERROR: '$target' does not exist." >&2; return 1; }
+      # shellcheck disable=SC2164
       command cd "$(command dirname -- "$target")" # Change to target dir; necessary for correct resolution of target path.
       fname=$(command basename -- "$target") # Extract filename.
-      [ "$fname" = '/' ] && fname='' # !! curiously, `basename /` returns '/'
+      [ "$fname" = '/' ] && fname='' # WARNING: curiously, `basename /` returns '/'
       if [ -L "$fname" ]; then
         # Extract [next] target path, which may be defined
-        # *relative* to the symlink's own directory.
-        # Note: We parse `ls -l` output to find the symlink target
-        #       which is the only POSIX-compliant, albeit somewhat fragile, way.
+        # relative to the symlink's own directory.
+        # NOTE: We parse `ls -l` output to find the symlink target
+        # NOTE:   which is the only POSIX-compliant, albeit somewhat fragile, way.
         target=$(command ls -l "$fname")
         target=${target#* -> }
         continue # Resolve [next] symlink target.
@@ -50,28 +65,35 @@ function rreadlink() ( # Execute the function in a *subshell* to localize variab
   done
   targetDir=$(command pwd -P) # Get canonical dir. path
   # Output the ultimate target's canonical path.
-  # Note that we manually resolve paths ending in /. and /.. to make sure we have a normalized path.
+  # NOTE: that we manually resolve paths ending in /. and /.. to make sure we have a normalized path.
   if [ "$fname" = '.' ]; then
     command printf '%s\n' "${targetDir%/}"
   elif  [ "$fname" = '..' ]; then
-    # Caveat: something like /var/.. will resolve to /private (assuming /var@ -> /private/var), i.e. the '..' is applied
-    # AFTER canonicalization.
+    # NOTE: something like /var/.. will resolve to /private (assuming /var@ -> /private/var), 
+    # NOTE:   i.e. the '..' is applied AFTER canonicalization.
     command printf '%s\n' "$(command dirname -- "${targetDir}")"
   else
     command printf '%s\n' "${targetDir%/}/$fname"
   fi
 )
 
-SETUP_DIR=$(dirname -- "$(rreadlink "$0")")
+#endregion Functions
+################################################################################
+
+################################################################################
+#region Immediate
+
+MY_DIR=$(dirname -- "$(rreadlink "$0")")
 
 (
-  cd "$HOME"
-  ln -s .env_vars_profile "${SETUP_DIR}"/.env_vars_profile
-  ln -s .vimrc "${SETUP_DIR}"/.vimrc
-  ln -s .zshrc "${SETUP_DIR}"/.zshrc
+  cd "$HOME" || exit 1
+  ln -sf .env_vars_profile "${MY_DIR}"/.env_vars_profile
+  ln -sf .vimrc "${MY_DIR}"/.vimrc
+  ln -sf .zshrc "${MY_DIR}"/.zshrc
 
+  # TODO: insert missing lines
   if [ ! -f .gitconfig ]; then
-    cp "${SETUP_DIR}"/.gitconfig-base .gitconfig
+    cp "${MY_DIR}"/.gitconfig-base .gitconfig
   fi
 
   exit 0
@@ -79,3 +101,5 @@ SETUP_DIR=$(dirname -- "$(rreadlink "$0")")
 ret=$?
 exit $ret
 
+#endregion Immediate
+################################################################################
